bdcf7e84d5b1415e714ce4f19d3f2fb8ccf0956f
Hwang Seunghwan <xx0hnhwang@gmail.com>
2021-08-02 18:09:18 +0900

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ const {logger} = require('./winston');
 
 // TODO: 본인의 DB 계정 입력
 const pool = mysql.createPool({
-    host: 'database-1.cohwvksyim93.ap-northeast-2.rds.amazonaws.com',
-    user: 'xx0hn',
+    host: '',
+    user: '',
     port: '3306',
-    password: 'hsh970412',
-    database: 'RC_webservice'
+    password: '',
+    database: ''
 });
 
 module.exports = {
